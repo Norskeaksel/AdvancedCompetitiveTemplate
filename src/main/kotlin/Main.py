1a191db35ada337bbe60995f68642ba89cd84041
Aksel <ahsorbye@gmail.com>
2023-10-07 20:25:03 +0200

    Complete python setup

--- src/main/kotlin/Main.py
+++ src/main/kotlin/Main.py
@@ -3,9 +3,21 @@ def solve(a, b):
 
 
 if __name__ == "__main__":
+    try:
+        testInput = open('Input.txt')
+        output = open('Output.txt', 'w')
+    except FileNotFoundError:
+        output = None
     while True:
-        a, b = [int(i) for i in input().split()]
+        try:
+            a, b = [int(i) for i in testInput.readline().split()]
+        except:
+            a, b = [int(i) for i in input().split()]
+
         if a + b == 0:
             break
+
+        if output is not None:
+            output.write(str(solve(a, b)) + '\n')
 
         print(solve(a, b))
