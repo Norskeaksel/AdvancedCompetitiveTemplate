b3e642ce2a228c71d3e463d59e2f9be763a23379
Aksel <ahsorbye@gmail.com>
2023-10-07 17:31:47 +0200

    Prepare 4 merge

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,14 @@
-import java.io.File
 import java.io.PrintWriter
+import java.math.BigInteger
 import java.util.*
 
+// val INPUT = File("src/main/kotlin/Input.txt").inputStream()
 @JvmField
-val INPUT = File("src/main/kotlin/Input.txt").inputStream()
-// val INPUT = System.`in`
+val INPUT = System.`in`
 
+// val OUTPUT = File("src/main/kotlin/Output.txt").outputStream()
 @JvmField
-val OUTPUT = File("src/main/kotlin/Output.txt").outputStream()
-// val OUTPUT = System.out
+val OUTPUT = System.out
 
 @JvmField
 val _reader = INPUT.bufferedReader()
@@ -25,26 +25,33 @@ fun read(): String {
 @JvmField
 val _writer = PrintWriter(OUTPUT, false)
 
-fun readInt() = read().toInt()
-fun readDouble() = read().toDouble()
-fun readLong() = read().toLong()
 fun readStrings(n: Int) = List(n) { read() }
-fun readLines(n: Int) = List(n) { readString() }
-fun readInts(n: Int) = List(n) { read().toInt() }
-fun readIntArray(n: Int) = IntArray(n) { read().toInt() }
-fun readDoubles(n: Int) = List(n) { read().toDouble() }
-fun readDoubleArray(n: Int) = DoubleArray(n) { read().toDouble() }
-fun readLongs(n: Int) = List(n) { read().toLong() }
-fun readLongArray(n: Int) = LongArray(n) { read().toLong() }
-fun readFileLines(fileName: String) = File(fileName).readLines()
-fun debug(x: Any) = System.err.println("DEBUG: $x")
 
 fun main() {
-    _writer.solve(); _writer.flush()
+    _writer.run(); _writer.flush()
 }
 
-fun PrintWriter.solve() {
-    val n = readInt()
-    repeat(n) {
+fun PrintWriter.run() {
+    while (true) {
+        val (a, b) = readStrings(2)
+        if (a == "0" && b == "0") break
+        println(solve(a, b))
     }
+}
+
+fun solve(a: String, b: String): Int {
+    val A = a.toBigInteger()
+    val B = b.toBigInteger()
+    var prev2 = BigInteger.ZERO
+    var prev1 = BigInteger.ONE
+    var nr = 0
+    while (prev2 + prev1 <= B) {
+        if (prev2 + prev1 >= A) {
+            nr++
+        }
+        val cur = prev2 + prev1
+        prev2 = prev1
+        prev1 = cur
+    }
+    return nr
 }
